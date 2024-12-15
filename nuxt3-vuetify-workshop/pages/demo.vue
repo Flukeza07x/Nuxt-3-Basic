db6f4c8ae1bfdb6acd93ffa1b2d610b5e36240a5
Flukeza07x <mr.fluke2544@gmail.com>
2024-12-15 15:38:24 +0700

    add day4 nuxt vercel
    add day4 nuxt vercel

---
+++ nuxt3-vuetify-workshop/pages/demo.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mt-5">
+    <h1 class="mt-10">Demo page</h1>
+<p  class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat facilis laborum ipsam natus accusantium ducimus sint velit perspiciatis voluptas doloribus?</p>
+
+  </div>
+</template>
+
+<style scoped></style>
